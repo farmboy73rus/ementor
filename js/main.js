1d4391ad9d31cd8f08edfa72c3f2ee91537119db
Alex Krasnov <farmboy73rus@gmail.com>
2022-11-30 21:01:18 +0400

    Fix bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const scrollToDownLink = document.querySelector('#scroll_down_link');
 const switchButton = document.querySelectorAll('.js-switch');
 const menuToggle = document.querySelector('#menu_toggle');
+const animatedItems = document.querySelectorAll('.animation');
 
 const noScrollHandler = () => {
     if (menuToggle.checked == true) {
@@ -35,3 +36,23 @@ if (scrollToDownLink) {
 }
 
 menuToggle.addEventListener('change', noScrollHandler);
+
+if (animatedItems.length > 0) {
+    const animateOnScroll = () => {
+        for (let index = 0; index < animatedItems.length; index++) {
+            const animatedItem = animatedItems[index];
+            const animatedItemHeight = animatedItem.offsetHeight;
+            const animatedItemOffset = animatedItem.getBoundingClientRect().top;
+
+            if (
+                ((animatedItemOffset + animatedItemHeight) < window.innerHeight) &&
+                ((animatedItemOffset + animatedItemHeight) > 0))
+            {
+                animatedItem.classList.add('animation--slidetoup');
+            } else {
+                animatedItem.classList.remove('animation--slidetoup');
+            }
+        }
+    }
+    window.addEventListener('scroll', animateOnScroll);
+}
